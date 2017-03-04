dca7c4e0c9cb64811c4f9a1f92226e2540803b11
Ashton Morgan <ashtomorgan3@gmail.com>
2017-03-03 20:21:29 -0600

    Initial Commit

---
+++ YoutubeClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutubeClone
+//
+//  Created by Ashton Morgan on 3/3/17.
+//  Copyright © 2017 Ashton Morgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
